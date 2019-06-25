6429ae9f01f7deb1d8389eef4dea1c0f35b0c6ae
Ben Norskov <norskovb@newschool.edu>
2019-06-25 21:10:12 +0800

    first attempt at adding student files

--- globalJS.js
+++ globalJS.js
@@ -3,24 +3,30 @@ var myImages = [];
 var maxNumImages = 58;
 var sequencers = [];
 var characterNavs;
-var doAnimate = true;
+var doAnimate = false;
 var imageFrames = [];
 function ready() {
+	removeUnsetElements();
 	// Set up image file names
 	for (var i = 0; i<maxNumImages; i++) {
 		//myImages[i] = new Image(400, 400);
 		var num = i;
 		if (i<10) num = "0"+i;	
-		myImages[i] = "globalImages/version2/000"+num+".png"
+		myImages[i] = "../../globalImages/version2/000"+num+".png"
 	}
 	characterNavs = document.getElementsByClassName("characterNavigation");
 	for (var i = characterNavs.length - 1; i >= 0; i--) {
-		sequencers[i] = document.createElement("img");
-		sequencers[i].src = myImages[0];
-		imageFrames[i] = Math.floor( Math.random() * maxNumImages);
-		characterNavs[i].replaceChild(sequencers[i], characterNavs[i].firstChild);
+		if(characterNavs[i].getAttribute("href").indexOf("studentname") != -1) {
+			characterNavs[i].remove();
+		} else {
+			doAnimate = true;
+			sequencers[i] = document.createElement("img");
+			sequencers[i].src = myImages[0];
+			imageFrames[i] = Math.floor( Math.random() * maxNumImages);
+			characterNavs[i].replaceChild(sequencers[i], characterNavs[i].firstChild);
+		}
 	};
-	if (characterNavs.length > 0) window.requestAnimationFrame(animate); 
+	if (characterNavs.length > 0 && doAnimate) window.requestAnimationFrame(animate); 
 }
 
 var lastAnimationTime = 0;
@@ -36,4 +42,16 @@ function animate() {
 		};
 	}
 	window.requestAnimationFrame(animate);
+}
+function removeUnsetElements() {
+	var characterInfo = document.getElementById("characterInfo");
+	if (characterInfo.innerText.indexOf("This Character") != -1) characterInfo.remove();
+
+	var artistName = document.getElementById("artistName");
+	if (artistName.innerText.indexOf("Norskov") != -1) artistName.remove();
+
+	var imageDescription = document.getElementById("imageDescription");
+	if (imageDescription.innerText.indexOf("Cityscape") != -1) {
+		imageDescription.remove();
+	}
 }
